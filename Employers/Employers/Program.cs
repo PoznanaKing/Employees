e7704fb737a5149e8ad2197b616f7f11f1272538
PoznanaKing <pozsgain@kkszki.hu>
2024-09-22 14:09:36 +0200

    Minden dolgozó felvéve, kiírva az összes dolgozó neve, a legjobban kereső meghatározva.

---
+++ Employers/Employers/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Permissions;
+
+namespace Employers
+{
+    class Employee //2. feladat összetett adatszerkezet.
+    {
+        private int id;
+        private string name;
+        private int age;
+        private int earnings;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public int Earnings
+        {
+            get { return earnings; }
+            set { earnings = value; }
+        }
+    }
+    internal class Program
+    {
+        static List<Employee> employees = new List<Employee>();
+        static void Main(string[] args)
+        {
+            //1. feladat
+            StreamReader sr = new StreamReader("tulajdonsagok_100sor.txt");
+            while (!sr.EndOfStream)//a dolgozók hozzáadása az employees listához.
+            {
+                string[] temp = sr.ReadLine().Split(';');
+                Employee emp = new Employee();
+                emp.Id= Convert.ToInt32(temp[0]);
+                emp.Name= temp[1];
+                emp.Age= Convert.ToInt32(temp[2]);
+                emp.Earnings= Convert.ToInt32(temp[3]);
+                employees.Add(emp);
+            }
+            sr.Close();
+
+            foreach (var item in employees)// 3. feladat: megjelenítem a dolgozók nevét.
+            {
+                Console.WriteLine($"{item.Name}");
+            }
+
+            //4. feladat: a legjobban keresők nevének és azonosítójának kiírása.
+            
+            int max = 0;
+            foreach (var item in employees)
+            {
+                if (max <= item.Earnings)
+                {
+                    max = item.Earnings;
+                }
+            }
+            foreach (var item in employees)
+            {
+                if (item.Earnings>=max)
+                {
+                    Console.WriteLine($"\nAzonosító: {item.Id}\nNév: {item.Name}");
+                }
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Employers/Employers/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Employers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ReviOS 10 23.10")]
+[assembly: AssemblyProduct("Employers")]
+[assembly: AssemblyCopyright("Copyright © ReviOS 10 23.10 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f06e826a-428a-4127-bad0-b83d8c101025")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
